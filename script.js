d9660189f920b8383dfe613c0f706ea07d6cd734
1serj1 <103180265+1serj1@users.noreply.github.com>
2022-06-09 09:00:02 +0500

    1

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 
 document.addEventListener(`DOMContentLoaded`, () => { 
 
-
+const userName = prompt("Как к Вам обращатся?",""); 
+const userEmail = prompt("Введите Ваш  e mail","");
+//  function pusto(){
+//   if (userName) {  // если userName пустой, то фолс - цикл не пойдет - додумать, хоршая идея!
+//       alert(userName);
+// } else {
+//     const userEmail = prompt("Введите Ваш  e mail","")
+// }
+//  };
  const klientDB = {
 
-    klientName: prompt("Как к Вам обращатся?",""),
-    klientEmail: prompt("Введите Ваш  e mail","")
-
+    klientName: userName,
+    klientEmail: userEmail
 }; 
 console.log (klientDB);
 });
